0f8bef0dd0230426215abb8578f18cb8c3a4d5f4
Jason Rai <jrai@homex.com>
2019-02-27 10:29:59 -0500

    add shadow to arc

--- lib/arc.dart
+++ lib/arc.dart
@@ -1,20 +1,23 @@
+import 'package:clippy_flutter/src/clip_shadow.dart';
 import 'package:flutter/widgets.dart';
 import 'package:clippy_flutter/src/arc_clipper.dart';
 import 'package:clippy_flutter/src/edge.dart';
 
 export 'package:clippy_flutter/src/edge.dart';
 export 'package:clippy_flutter/src/arc_clipper.dart' show ArcType;
+export 'package:clippy_flutter/src/clip_shadow.dart' show ClipShadow;
 
 class Arc extends StatelessWidget {
-  const Arc({
-    Key key,
-    @required this.height,
-    @required this.child,
-    this.edge = Edge.BOTTOM,
-    this.arcType = ArcType.CONVEX,
-  }) : super(key: key);
+  const Arc(
+      {Key key,
+      @required this.height,
+      @required this.child,
+      this.edge = Edge.BOTTOM,
+      this.arcType = ArcType.CONVEX,
+      this.clipShadows = const []})
+      : super(key: key);
 
-  /// The widget which one of [edge]s is going to be clipped as arc
+  /// The widget which one of [edge]s is going to be clippddddded as arc
   final Widget child;
 
   ///The height of the arc
@@ -26,11 +29,18 @@ class Arc extends StatelessWidget {
   ///The type of arc which can be [ArcType.CONVEX] or [ArcType.CONVEY]
   final ArcType arcType;
 
+  ///List of shadows to be cast on the border
+  final List<ClipShadow> clipShadows;
+
   @override
   Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: ArcClipper(height, edge, arcType),
-      child: child,
+    var clipper = ArcClipper(height, edge, arcType);
+    return CustomPaint(
+      painter: ClipShadowPainter(clipper, clipShadows),
+      child: ClipPath(
+        clipper: clipper,
+        child: child,
+      ),
     );
   }
 }
